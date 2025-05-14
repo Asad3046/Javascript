bd0cb3ee695bfaac380c2f1fccc4ab209f958c49
Asad <asadmohammad8259@gmail.com>
2025-05-14 16:14:34 +0530

    new files added

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+let name = "Asad" // string datatype
+let age = 24 // number datatype
+let isLoggedIn = true // boolean datatype
+
+/*
+Types of datatypes
+1. number
+2. bigint
+3. string => ""
+4. boolean => true/false
+5. null => standalone value
+6. undefined => 
+7. symbols => unique
+*/
+
+console.log(typeof age);
+console.log(typeof null);
+
---
+++ variables.js
@@ -0,0 +1,19 @@
+const accountId = 4578
+let accountEmail = "asadmohammad8259@gmail.com"
+var accountPassword = "123456"
+let accountState;
+
+// accountId = 6542 //this is not allowed
+accountEmail = "mohammadasad7658@gmail.com"
+accountPassword = "654321"
+
+/*
+prefer not to use 'var' 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountState])
+
+
+
+
